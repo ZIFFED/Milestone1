21f8d57221d16bde443599d3bc8a58015a2fd36f
George Young <geoyoung@cs.du.edu>
2018-04-08 23:28:07 -0600

    Updated idle animation. Made physics more snappy. Cleaned up/organized all files.\n\n>Made gravity and sensitivity settings much higher, to make gameplay feel more quick.\n>Reorganized and added to the idle animation to make it more fluid.\n>Made two separate idle animations, one starts facing left and the other facing right.\n>Generally cleaned up and organized previous code

---
+++ Assets/Animations/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+	private float speed;
+	Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		speed = 2.5f;
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float axisX = Input.GetAxis ("Horizontal");
+		float axisY = Input.GetAxis ("Vertical");
+		if (Input.GetKey ("d")) 
+		{
+			anim.SetTrigger ("makeRunRight");
+		}
+		else if (Input.GetKey ("a")) 
+		{
+			anim.SetTrigger ("makeRunLeft");
+		}
+		else
+		{			
+			anim.SetTrigger ("makeIdle");
+		}
+
+		transform.Translate (new Vector3 (axisX, axisY) * (speed * Time.deltaTime));
+	}
+}
